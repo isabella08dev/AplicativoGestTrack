4a5515ba248026bb16befbef2dfe1f4fe0cdc7b4
IsabellaOliveira08 <isabellaoliveir08@gmail.com>
2025-10-14 16:50:39 -0300

    Atualização Paciente Notas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,16 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.5"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+    implementation("io.ktor:ktor-client-android:3.2.2")
+    implementation("io.ktor:ktor-client-android:3.2.2")
+    implementation("io.ktor:ktor-client-core:3.2.2")
+    implementation("io.ktor:ktor-client-content-negotiation:3.2.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.cardview:cardview:1.0.0")
+
 }
--- app/src/main/java/com/example/gesttrack/paciente/MinhasNotasPacienteActivity.kt
+++ app/src/main/java/com/example/gesttrack/paciente/MinhasNotasPacienteActivity.kt
@@ -1,4 +1,35 @@
 package com.example.gesttrack.paciente
 
-class MinhasNotasPacienteActivity {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gesttrack.R
+
+class MinhasNotasPacienteActivity : AppCompatActivity() {
+
+    private lateinit var notasPaciente: NotasPaciente
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paciente_minhasnotas)
+
+        val etNote = findViewById<EditText>(R.id.etNote)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val rvNotes = findViewById<RecyclerView>(R.id.rvNotes)
+
+        notasPaciente = NotasPaciente(mutableListOf())
+        rvNotes.layoutManager = LinearLayoutManager(this)
+        rvNotes.adapter = notasPaciente
+
+        btnAdd.setOnClickListener {
+            val note = etNote.text.toString()
+            if (note.isNotBlank()) {
+                notasPaciente.addNote(note)
+                etNote.text.clear()
+            }
+        }
+    }
 }
